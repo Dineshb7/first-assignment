f36c5d7a56c3896270ba57ca8c75d48424491fae
dinesh <dineshgitam078@gmail.com>
2020-09-08 02:11:22 +0530

    9th

--- src/server.js
+++ src/server.js
@@ -3,12 +3,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static('C:/Users/dinesh/Documents/first-app/first-assignment/dist/'));
+app.use(express.static(__dirname + '/first-assignment/dist'));
 app.get('/*', function(req,res) {
     
-res.sendFile(`C:/Users/dinesh/Documents/first-app/first-assignment/dist/first-assignment/index.html`);
+res.sendFile(__dirname + '/first-assignment/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
