1015e0a64a848e13839c218c8661701db92b9f46
dinesh <dineshgitam078@gmail.com>
2020-09-08 10:23:07 +0530

    16th

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.static(__dirname + '/first-assignment/dist'));
 app.get('/*', function(req,res) {
     
-res.sendFile(__dirname + '/first-assignment/dist/index.html');
+res.sendFile(__dirname + '/dist/index.html');
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
